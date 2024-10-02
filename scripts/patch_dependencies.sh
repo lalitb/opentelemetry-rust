0f496c633167030ded76f79a9ee8c7350f131175
Lalit Kumar Bhasin <lalit_fin@yahoo.com>
2024-10-02 13:26:22 -0700

    fix

--- scripts/patch_dependencies.sh
+++ scripts/patch_dependencies.sh
@@ -12,17 +12,27 @@ rust_version=$(rustc --version | cut -d' ' -f2)
 target_version="1.71.1"
 
   function patch_version() {
-    local latest_version=$(cargo search --limit 1 $1 | head -1 | cut -d'"' -f2)
-    echo "patching $1 from $latest_version to $2"
-    cargo update -p $1:$latest_version --precise $2
+    local package=$1
+    local new_version=$2
+    local crate_path=$3
+    local latest_version=$(cargo search --limit 1 $package | head -1 | cut -d'"' -f2)
+    if [ -n "$crate_path" ]; then
+        # If crate_path is provided, patch only for the specified crate
+        echo "-->Patching $package in $crate_path from $latest_version to $new_version"
+        cargo update -p $package:$latest_version --precise $new_version --manifest-path "$crate_path/Cargo.toml"
+    else
+        # If crate_path is not provided, patch for the entire workspace
+        echo "Patching $package in the entire workspace from $latest_version to $new_version"
+        cargo update -p $package:$latest_version --precise $new_version
+    fi
   }
 
   patch_version cc 1.0.105
   patch_version url 2.5.0
 if version_lt "$rust_version" "$target_version"; then
-  patch_version tonic 0.12.2
+  patch_version tonic 0.12.3 "opentelemetry-sdk"
   patch_version hyper-rustls 0.27.2 # 0.27.3 needs rustc v1.70.0
-  patch_version tokio-util 0.7.11 # 0.7.12 needs rustc v1.70.0
-  patch_version tokio-stream 0.1.15 # 0.1.16 needs rustc v1.70.0
-  patch_version tokio 1.38.0 # 1.39 needs msrv bump to rustc 1.70
+  patch_version tokio-util 0.7.11 "opentelemetry-sdk" # 0.7.12 needs rustc v1.70.0
+  patch_version tokio-stream 0.1.16 "opentelemetry-sdk" # 0.1.16 needs rustc v1.70.0
+  patch_version tokio 1.38.0 "opentelemetry-sdk" # 1.39 needs msrv bump to rustc 1.70
 fi
